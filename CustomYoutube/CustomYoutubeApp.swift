2b0af81ad4c6e4505780fa3f5d89f255f0e69872
Justin Snider <jsnider@newrelic.com>
2023-01-11 12:32:14 -0700

    Initial Commit

---
+++ CustomYoutube/CustomYoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomYoutubeApp.swift
+//  CustomYoutube
+//
+//  Created by Justin Snider on 1/11/23.
+//
+
+import SwiftUI
+
+@main
+struct CustomYoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
